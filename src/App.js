81aac1c1b9cfaa0b3c8dbbb477f7af301ac8062d
joje3029 <joje3029@gmail.com>
2025-01-27 17:24:04 +0900

    클론코딩 06 완료. 07 진입

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
+
 class App extends React.Component {
 	state = {
 		isLoding: true,
@@ -11,41 +13,40 @@ class App extends React.Component {
 	getMovies = async () => {
 		const {
 			data: {
-				//여기서 data ->
-				data: { movies }, //여기서 data -> movies 진행
+				data: { movies },
 			},
 		} = await axios.get(
 			"https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
 		);
 
-		this.setState({ movies, isLoding: false }); // 위의 데이터 movies를 stat의 movies에 넣고 isLoding을 false로 변경
+		this.setState({ movies, isLoding: false });
 	};
 
-	//컴포넌트 마운트 후 실행되는 함수(생명주기 함수)
 	componentDidMount() {
-		//영화 데이터 로딩이 완료되면 state에 저장
 		this.getMovies();
 	}
 
 	render() {
 		const { isLoding } = this.state;
 		return (
-			<div>
+			<section className="container">
 				{isLoding
 					? "Loading..."
 					: this.state.movies.map((movie) => {
-							// console.log(movie);
 							return (
-								<Movie
-									key={movie.id} //앞에서 본 컴포넌트마다 고유한 key
-									title={movie.title}
-									rating={movie.rating}
-									summary={movie.summary}
-									poster={movie.medium_cover_image}
-								/>
+								<div className="movies">
+									<Movie
+										key={movie.id}
+										title={movie.title}
+										year={movie.year}
+										rating={movie.rating}
+										summary={movie.summary}
+										poster={movie.medium_cover_image}
+									/>
+								</div>
 							);
 					  })}
-			</div>
+			</section>
 		);
 	}
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,24 @@
 import React from "react";
+import "./Movie.css";
 import PropTypes from "prop-types";
 
-function Movie({ id, title, rating, summary, poster }) {
-	return <h4>{title}</h4>;
+function Movie({ title, year, rating, summary, poster }) {
+	return (
+		<div className="movie">
+			<img src={poster} alt={title} title={title} />
+			<div className="movie__data">
+				<h3 className="movie__title">{title}</h3>
+				<h5 className="movie__year">{year}</h5>
+				<p className="movie__rating">{rating}</p>
+				<p className="movie__summary">{summary.slice(0, 180)}...</p>
+			</div>
+		</div>
+	);
 }
 
-//props로 app에서 데이터 받음.
 Movie.propTypes = {
-	id: PropTypes.number.isRequired,
 	title: PropTypes.string.isRequired,
+	year: PropTypes.number.isRequired,
 	rating: PropTypes.number,
 	summary: PropTypes.string,
 	poster: PropTypes.string,
