7dc5c04b4bf144f6c7dd70914779f589997358e6
joje3029 <joje3029@gmail.com>
2025-01-27 15:37:21 +0900

    클론코딩 05-02 까지. state 까지함.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
-import PropTypes from "prop-types";
 import React from "react";
-import Food from "./Potato";
 
-function App() {
-	const foodILike = [
-		{
-			id: 1,
-			name: "kimchi",
-			rating: 5,
-			image: "https://sugaryums.com/wp-content/uploads/2023/03/Baechu-Kimchi-Cabbage-Kimchi-Recipe-SugarYums-17.jpg",
-		},
-		{
-			id: 2,
-			name: "samgiopsal",
-			rating: 4.9,
-			image: "https://english.visitkorea.or.kr/public/images/foodtrip/k-food-img/img_kfood_view_107.jpg",
-		},
-		{
-			id: 3,
-			name: "chukumi",
-			rating: 4.8,
-			image: "https://tblg.k-img.com/resize/640x360c/restaurant/images/Rvw/249684/107a648f49bc7889f2a28bc5a9367c10.jpg?token=3d0cb09&api=v2",
-		},
-		{
-			id: 4,
-			name: "bibimbap",
-			rating: 4.7,
-			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB32qrCC4gzonTMGeXpoX9jUO8QSj2dQsNQA&s",
-		},
-		{
-			id: 5,
-			name: "donkatsu",
-			rating: 4.6,
-			image: "https://mykoreankitchen.com/wp-content/uploads/2023/08/0.-Donkatsu.jpg",
-		},
-	]; //여기서 데이터 넘어옴 , 서버 대용
+class App extends React.Component {
+	/*클래스 내부에서는 const, let 같은 변수 선언 키워드 사용 안함. 클래스 문법 규칙 때문,
+	  클래스는 개체의 설계도 이므로, 내부의 속성과 메소드는 클래스의 일부로 정의
+	  클래스 필드와 메서드는 이미 클래스에 속해있기 때문에 별도의 선언 필요 안함.
+	  이는 Java나 다른 객체지향 언어의 클래스 문법과 유사
+	*/
 
-	Food.propTypes = {
-		name: PropTypes.string.isRequired, // isRequired는 필수 속성이라는 뜻 == 없으면 안됨.
-		rating: PropTypes.number.isRequired,
-		image: PropTypes.string.isRequired,
+	//state는 객체 형태의 데이터. stste를 사용하려면 반드시 클래스형 컴포넌트 안에서, 소문자를 이용하여 state라고 적음.
+	state = {
+		count: 0,
 	};
 
-	return (
-		<div>
-			<h1>Hello World</h1>
-			{foodILike.map((dish) => (
-				<Food key={dish.id} name={dish.name} image={dish.image} />
-			))}
-			{/* key는 리액트의 props 고유성을 구분하기 위한것. 왜 그래서 vue에서도 v-for 쓰면 key값 하잖아. */}
-		</div>
-	);
+	add = () => {
+		this.setState((current) => {
+			console.log("current.count : ", current);
+			return { count: current.count + 1 };
+		});
+	};
+
+	minus = () => {
+		this.setState((current) => ({ count: current.count - 1 }));
+	};
+
+	//클래스형 컴포넌트에서는 JSX 를 반환하기 위해 render() 메서드를 사용해야 함.
+	render() {
+		return (
+			<div>
+				<h1>I'm a class component</h1>
+				<div> The number is : {this.state.count}</div>
+				<button onClick={this.add}>Add</button>
+				<button onClick={this.minus}>Minus</button>
+			</div>
+		);
+	}
 }
 
 export default App;
