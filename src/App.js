8eaf50f665dbf5fec3a73a178c4bc1445499526d
joje3029 <joje3029@gmail.com>
2025-01-27 14:09:18 +0900

    클론코딩 04-04까지 함.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from "react";
 import Food from "./Potato";
 
 function App() {
-	const food = [
+	const foodILike = [
 		{
+			id: 1,
 			name: "kimchi",
 			image: "https://sugaryums.com/wp-content/uploads/2023/03/Baechu-Kimchi-Cabbage-Kimchi-Recipe-SugarYums-17.jpg",
 		},
 		{
+			id: 2,
 			name: "samgiopsal",
 			image: "https://english.visitkorea.or.kr/public/images/foodtrip/k-food-img/img_kfood_view_107.jpg",
 		},
 		{
+			id: 3,
 			name: "chukumi",
 			image: "https://tblg.k-img.com/resize/640x360c/restaurant/images/Rvw/249684/107a648f49bc7889f2a28bc5a9367c10.jpg?token=3d0cb09&api=v2",
 		},
 		{
+			id: 4,
 			name: "bibimbap",
 			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB32qrCC4gzonTMGeXpoX9jUO8QSj2dQsNQA&s",
 		},
 		{
+			id: 5,
 			name: "donkatsu",
 			image: "https://mykoreankitchen.com/wp-content/uploads/2023/08/0.-Donkatsu.jpg",
 		},
@@ -28,10 +33,10 @@ function App() {
 	return (
 		<div>
 			<h1>Hello World</h1>
-			{/* 여기를 잘 이해하고 알아야 겠네. */}
-			{food.map((dish) => (
-				<Food name={dish.name} image={dish.image} />
+			{foodILike.map((dish) => (
+				<Food key={dish.id} name={dish.name} image={dish.image} />
 			))}
+			{/* key는 리액트의 props 고유성을 구분하기 위한것. 왜 그래서 vue에서도 v-for 쓰면 key값 하잖아. */}
 		</div>
 	);
 }
