54d3227c232ae29863c27c800f65dfafa93b811d
joje3029 <joje3029@gmail.com>
2025-01-27 13:57:47 +0900

    클론코드 04. map을 이용한 컴포넌트 반복 - 이거 이해 잘되야겠네

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React from "react";
 import Food from "./Potato";
 
 function App() {
+	const food = [
+		{
+			name: "kimchi",
+			image: "https://sugaryums.com/wp-content/uploads/2023/03/Baechu-Kimchi-Cabbage-Kimchi-Recipe-SugarYums-17.jpg",
+		},
+		{
+			name: "samgiopsal",
+			image: "https://english.visitkorea.or.kr/public/images/foodtrip/k-food-img/img_kfood_view_107.jpg",
+		},
+		{
+			name: "chukumi",
+			image: "https://tblg.k-img.com/resize/640x360c/restaurant/images/Rvw/249684/107a648f49bc7889f2a28bc5a9367c10.jpg?token=3d0cb09&api=v2",
+		},
+		{
+			name: "bibimbap",
+			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB32qrCC4gzonTMGeXpoX9jUO8QSj2dQsNQA&s",
+		},
+		{
+			name: "donkatsu",
+			image: "https://mykoreankitchen.com/wp-content/uploads/2023/08/0.-Donkatsu.jpg",
+		},
+	]; //여기서 데이터 넘어옴 , 서버 대용
+
 	return (
 		<div>
 			<h1>Hello World</h1>
-			<Food
-				fav="kimchi"
-				something={true}
-				papapapa={["kimchi", "ramen", 1, 2, 3]}
-			/>
-			<Food fav="ramen" />
-			<Food fav="samgiopsal" />
-			<Food fav="chukumi" />
+			{/* 여기를 잘 이해하고 알아야 겠네. */}
+			{food.map((dish) => (
+				<Food name={dish.name} image={dish.image} />
+			))}
 		</div>
 	);
 }
