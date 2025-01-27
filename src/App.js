708bd66ab686f7d0f503a0b370959e9253e07c61
joje3029 <joje3029@gmail.com>
2025-01-27 16:57:33 +0900

    클론코딩 06-2까지 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
 	state = {
@@ -6,12 +7,22 @@ class App extends React.Component {
 		movies: [],
 	};
 
+	//구조 분해 할당
+	getMovies = async () => {
+		const {
+			data: {
+				//여기서 data ->
+				data: { movies }, //여기서 data -> movies 진행
+			},
+		} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+
+		this.setState({ movies, isLoding: false }); // 위의 데이터 movies를 stat의 movies에 넣고 isLoding을 false로 변경
+	};
+
 	//컴포넌트 마운트 후 실행되는 함수(생명주기 함수)
 	componentDidMount() {
 		//영화 데이터 로딩이 완료되면 state에 저장
-		setTimeout(() => {
-			this.setState({ isLoding: false });
-		}, 6000);
+		this.getMovies();
 	}
 
 	render() {
