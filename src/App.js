89b31774c07bafb52d8a4d0ef44b22e6e84e63cc
smpark <smpark@cuuva.com>
2025-01-31 13:04:03 +0900

    Movie.js 1차 리팩토링

--- src/App.js
+++ src/App.js
@@ -6,32 +6,6 @@ import "./App.css";
 import Detail from "./routes/Detail";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
-/*기존 코드
-function App() {
-	return (
-		<HashRouter>
-			<Navigation />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/movie-detail" element={<Detail />} />
-			</Routes>
-		</HashRouter>
-	);
-}
-*/
-
-/* 1차 라우트 설정 부분 분리
-const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/movie-detail" element={<Detail />} />
-    </Routes>
-  );
-};
-*/
 
 const routeConfig = [
 	{ path: "/", element: <Home /> },
@@ -39,19 +13,6 @@ const routeConfig = [
 	{ path: "/movie-detail", element: <Detail /> },
 ];
 
-/* 2차 라우트 설정 부분 분리
-const AppRouter = () => {
-    return (
-        <Routes>
-            {routeConfig.map((route) => (
-                <Route key={route.path} {...route} />
-            ))}
-        </Routes>
-    );
-};
-*/
-
-// 3차 라우트 설정 부분 분리 : 화살표 함수의 암시적 반환 사용
 const AppRouter = () => (
 	<Routes>
 		{routeConfig.map((route) => (
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,12 @@ import "../components/Movie.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Movie({ title, year, rating, summary, poster, genres }) {
+/*
+const Movie = ({ title, year, rating, summary, poster, genres }) => {
 	return (
 		<div className="movie">
 			<Link
-				to="/movie-detail" // 클릭시 http://localhost:3000/#/movie-detail 즉, 해당 영화의 상세보기로 이동,
+				to="/movie-detail"
 				state={{ year, title, summary, poster, genres }}
 			>
 				<img src={poster} alt={title} title={title} />
@@ -16,7 +17,6 @@ function Movie({ title, year, rating, summary, poster, genres }) {
 					<h5 className="movie__year">{year}</h5>
 					<ul className="movie__genres">
 						{genres.map((genre, index) => (
-							//여기도 map을 사용했고 해당 각 결과의 고유 키가 필요
 							<li key={index} className="genres__genre">
 								{genre}
 							</li>
@@ -28,7 +28,92 @@ function Movie({ title, year, rating, summary, poster, genres }) {
 			</Link>
 		</div>
 	);
-}
+};
+
+Movie.propTypes = {
+	title: PropTypes.string.isRequired,
+	year: PropTypes.number.isRequired,
+	rating: PropTypes.number,
+	summary: PropTypes.string,
+	poster: PropTypes.string,
+	genres: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default Movie;
+*/
+
+
+// MovieGenres 컴포넌트 분리
+const MovieGenres = ({ genres }) => (
+	<ul className="movie__genres">
+		{genres.map((genre, index) => ( //key={index} 대신 key={genre-${genre}}를 사용하여 더 안정적인 키 값 제공
+			<li key={`genre-${genre}`} className="genres__genre">
+				{genre}
+			</li>
+		))}
+	</ul>
+);
+
+/*
+key={index}의 문제점 : 배열의 인덱스를 key로 사용하면 배열이 변경될때, 해당 key 값이 변경됨.
+이로 인해 React가 불필요한 리렌더링을 하거나 예상치 못한 동작을 할 수 있음.
+     // 초기 genres 배열
+     ['액션', '코미디', '로맨스']  
+     // 액션(key=0), 코미디(key=1), 로맨스(key=2)
+
+     // '액션'이 제거된 genres 배열
+     ['코미디', '로맨스']
+     // 코미디(key=0), 로맨스(key=1)
+
+key={`genre-${genre}`}의 장점 : - 장르 이름 자체를 key에 포함시키므로 고유한 값을 가짐
+=> 배열이 변경되어도 각 항목의 key 값은 변하지 않음. React가 DOM 업데이트를 더 효율적으로 처리할 수 있음.
+
+// 초기 genres 배열
+['액션', '코미디', '로맨스']
+// genre-액션, genre-코미디, genre-로맨스
+// '액션'이 제거된 genres 배열
+['코미디', '로맨스']
+// genre-코미디, genre-로맨스
+
+*/
+
+
+// MovieInfo 컴포넌트 분리
+const MovieInfo = ({ title, year, rating, summary }) => (
+	<div className="movie__data">
+		<h3 className="movie__title">{title}</h3>
+		<h5 className="movie__year">{year}</h5>
+		<p className="movie__rating">{rating}</p>
+		<p className="movie__summary">
+			{summary.length > 180 ? `${summary.slice(0, 180)}...` : summary}
+		</p>
+	</div>
+);
+
+const Movie = ({ title, year, rating, summary, poster, genres }) => (
+	<div className="movie">
+		<Link
+			to="/movie-detail"
+			state={{ year, title, summary, poster, genres }}
+		>
+			<img src={poster} alt={title} title={title} />
+			<MovieInfo title={title} year={year} rating={rating} summary={summary} />
+			<MovieGenres genres={genres} />
+		</Link>
+	</div>
+);
+
+// PropTypes 정의
+MovieGenres.propTypes = {
+	genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+MovieInfo.propTypes = {
+	title: PropTypes.string.isRequired,
+	year: PropTypes.number.isRequired,
+	rating: PropTypes.number,
+	summary: PropTypes.string,
+};
 
 Movie.propTypes = {
 	title: PropTypes.string.isRequired,
