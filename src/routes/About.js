ca3de826044edb4d5cfc1d3dab3132c0ff1a0783
smpark <smpark@cuuva.com>
2025-01-31 14:07:14 +0900

    About.js 리팩토링

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./About.css";
 
-function About() {
-	return (
-		<div className="about__container">
-			<span>
-				“Freedom is the freedom to say that two plus two make four. If
-				that is granted, all else follows.”
-			</span>
-			<span>- George Orwell, 1984</span>
-		</div>
-	);
-}
+//현재 변경 안함으로 상수로 선언
+const QUOTE = {
+	text: "Freedom is the freedom to say that two plus two make four. If that is granted, all else follows.",
+	author: "George Orwell",
+	work: "1984"
+};
+
+const AboutContent = () => (
+	<div className="about__container">
+		<span>"{QUOTE.text}"</span>
+		<span>- {QUOTE.author}, {QUOTE.work}</span>
+	</div>
+);
+
+const About = () => {
+	return <AboutContent />;
+};
 
 export default About;
